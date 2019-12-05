a95c8032968da0bcb78139639b80114b08255cb1
Camilla Gejl Olsen <cami32d9@stud.kea.dk>
2019-12-05 22:07:44 +0100

    Added comments to JavaScript

--- script.js
+++ script.js
@@ -108,16 +108,17 @@ function toggleHoldButtons(wheels, spins) {
             button.addEventListener("click", holdButtonsListeners[holdWheelID]);
         }
 
-        // Removes eventListeners from the hold buttons after the last spin.
+        // Removes eventListeners from the hold buttons after the last spin, or after win, where spins is set to 0.
         if (spins === 0) {
             button.removeEventListener("click", holdButtonsListeners[holdWheelID]);
         }
     });
 }
 
-
 // Main spin function on click
 function spin(wheels, spins) {
+
+    // Stores the previously active symbol index, so it can be used to see how many times the DOM wheel should spin.
     wheels.forEach(wheel => {
         wheel.previouslyActive = wheel.active;
     });
@@ -130,27 +131,30 @@ function spin(wheels, spins) {
 
     spins--;
 
-    // If didWin returns true, the user will get the price and the start button will be activated.
+    // If the user wins:
     if (didWin) {
+
+        // Sets spins to 0 and toggles hold buttons, so they are deactivated.
         spins = 0;
         toggleHoldButtons(wheels, spins);
+
+        // Gets the prices from the active symbols.
         payPrice(wheels, spinResult);
+
+        // Activates the start button, so the user can start a new game.
         activateStartButton(wheels);
     }
 
-
-
-    // This starts the visual part of spinning the wheels, separately for each wheel.
+    // Starts the visual part of spinning the wheels, separately for each wheel.
     wheels.forEach(wheel => {
-        spinButtonClick(wheel);
+        spinVisualWheel(wheel);
     });
 
+    // Toggles the hold buttons.
     toggleHoldButtons(wheels, spins);
 
-    // If didWin returns false, the user will have one less spin left.
+    // If the user loses:
     if (!didWin) {
-        console.log("You didn't win!");
-
 
         // If spins is still above 0, the spin button will get activated again.
         if (spins > 0) {
@@ -163,10 +167,11 @@ function spin(wheels, spins) {
             activateStartButton(wheels);
         }
     }
-
 }
 
-// Return functions
+
+// ----- RETURN FUNCTIONS -----
+// Commented in the functions where they are used.
 
 function calculateSpinResult(wheels) {
     let spinResult = [];
@@ -191,6 +196,7 @@ function payPrice(wheels, spinResult) {
 
 // ----- VISUAL WHEELS -----
 
+// Adds the wheels, their symbols and hold buttons to the DOM.
 function addWheelsToDOM(wheels) {
     wheels.forEach(wheel => {
         document.querySelector(".wheels").innerHTML += `<div class="wheel wheel_${wheel.id}"></div>`;
@@ -203,8 +209,14 @@ function addWheelsToDOM(wheels) {
     })
 }
 
-function spinButtonClick(wheel) {
+function spinVisualWheel(wheel) {
+    // Only spins each wheel if they are not on hold.
     if (!wheel.isHolding) {
+
+        // To calculate how many times the wheel should spin to land on the active index, we take the current position
+        // of the wheel (the previously active index) - the new position where it should land (the active index). For
+        // the wheel to spin several rounds, take the wheel length * 2, and * the wheel.id so the wheels will stop
+        // one after one.
         let spinRounds = wheel.previouslyActive - wheel.active + (wheel.symbols.length * 2 * wheel.id);
         console.log(wheel.id + " Active " + (wheel.active));
 
@@ -214,6 +226,8 @@ function spinButtonClick(wheel) {
 
 function spinWheel(wheel, spinRounds) {
     document.querySelectorAll(`.wheel_${wheel.id} .item`).forEach(item => {
+
+        // Moves all symbols down 100% of their height.
         item.style.transitionDuration = ".05s";
         item.style.transform = "translateY(100%)";
     });
@@ -225,15 +239,24 @@ function spinWheel(wheel, spinRounds) {
 }
 
 function moveLastItem(wheel, spinRounds) {
+
+    // Moves all symbols back to their original position (which will change because there is another div added at the
+    // top - as happens below).
     document.querySelectorAll(`.wheel_${wheel.id} .item`).forEach(item => {
         item.style.transitionDuration = "0s";
         item.style.transform = "translateY(0)";
     });
 
+    // Variable that contains all the divs in a wheel.
     const allItems = document.querySelectorAll(`.wheel_${wheel.id} .item`);
+
+    // Finds the last div in the wheel by taking the amount of divs in the wheel.
     const lastItem = allItems[allItems.length - 1];
+
+    // Gets the ID from the last div, so it can be added to the top below.
     const lastSymbolID = lastItem.getAttribute("data-symbol-id");
 
+    // Removes the last div completely from the DOM.
     lastItem.parentNode.removeChild(lastItem);
 
     addLastItem(wheel, lastSymbolID, spinRounds);
@@ -241,11 +264,13 @@ function moveLastItem(wheel, spinRounds) {
 
 function addLastItem(wheel, lastSymbolID, spinRounds) {
 
+    // Inserts div to the start of the wheel, with the ID from the div removed from the end of the wheel.
     let lastItemTemplate = `<div class="item" data-symbol-id="${lastSymbolID}"></div>`;
     document.querySelector(`.wheel_${wheel.id}`).insertAdjacentHTML('afterbegin', lastItemTemplate);
 
     spinRounds--;
 
+    // If spinRounds is over 0, the functions will loop and the wheel will keep spinning until spinRounds hits 0.
     if (spinRounds > 0) {
         setTimeout(function () {
             spinWheel(wheel, spinRounds)
